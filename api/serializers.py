7b81c71c6fe7ba637317a31cfd32f00c1a2fdfd4
AlekseOne <solodko2001@mail.ru>
2023-09-06 22:06:21 +0300

    bug fix

--- api/serializers.py
+++ api/serializers.py
@@ -80,7 +80,6 @@ class HoweWorkSerializer(serializers.ModelSerializer):
 
 
 class AboutAsSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(max_length=None, use_url=True)
 
     class Meta:
         model = AboutAs
