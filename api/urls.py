0552e59d3c0c6a085849aaeea1bbccc1257a80e5
AlekseOne <solodko2001@mail.ru>
2023-09-07 20:39:52 +0300

    Добавил курсу и api

--- api/urls.py
+++ api/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from django.conf.urls.static import static
 from holmstroy import settings
 from .views import CombinedAPIView, FeedbackPostCreateView, CombinedDataView
+from .shema import urlpatterns as spectacular_urls
 
 urlpatterns = [
     path('pagedata/', CombinedAPIView.as_view(), name='combined-api-view'),
     path('main/', CombinedDataView.as_view(), name='pagedata'),
-
     path('create-feedback/', FeedbackPostCreateView.as_view(), name='create-feedback'),
-
 ]
-
+urlpatterns += spectacular_urls
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- holmstroy/settings.py
+++ holmstroy/settings.py
@@ -44,22 +44,28 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'drf_spectacular',
+    'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = []
 ROOT_URLCONF = 'holmstroy.urls'
 CORS_ALLOW_ALL_ORIGINS = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 TEMPLATES = [
     {
